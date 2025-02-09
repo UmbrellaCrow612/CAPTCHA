cdb53f6657f9bde2ff29f968b3ad336b5f2c2140
Yousaf Wazir <umbrellaman612@gmail.com>
2025-02-09 12:39:33 +0000

    finally have decent tile dialog
    - Whts left is click functionality and updating UI
    - sending captcha and sotring if verified result

--- web-ui/src/app/components/tile-captcha-dialog/tile-captcha-dialog.component.ts
+++ web-ui/src/app/components/tile-captcha-dialog/tile-captcha-dialog.component.ts
@@ -1,13 +1,50 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-tile-captcha-dialog',
-  imports: [MatDialogModule, MatFormFieldModule, MatButtonModule, NgOptimizedImage, MatButtonModule],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatButtonModule,
+  ],
   templateUrl: './tile-captcha-dialog.component.html',
   styleUrl: './tile-captcha-dialog.component.css',
 })
-export class TileCaptchaDialogComponent {}
+export class TileCaptchaDialogComponent implements OnInit {
+  isLoading = true;
+  data: captchaData = { id: '', imgUrl: '', matrix: [] };
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.isLoading = true;
+    this.httpClient
+      .get('https://localhost:7153/captcha/tile', {
+        responseType: 'blob',
+        observe: 'response',
+      })
+      .subscribe((res) => {
+        const imageUrl = URL.createObjectURL(res.body as Blob);
+        const matrix = JSON.parse(res.headers.get('X-Base-Matrix') ?? '[]');
+        const id = res.headers.get('X-Captcha-Id') ?? 'Null';
+
+        this.data.imgUrl = imageUrl;
+        this.data.matrix = matrix;
+        this.data.id = id;
+        this.isLoading = false;
+      });
+  }
+}
+
+interface captchaData {
+  imgUrl: string;
+  matrix: number[][];
+  id: string;
+}
